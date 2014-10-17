c0b723b52def5c212c5380834b0c5ae78fec2ad1
ma-te-us <mateus.ap@gmail.com>
2014-10-17 00:03:38 -0300

    Toying around in light table

--- project.clj
+++ project.clj
@@ -1,9 +1,10 @@
 (defproject desafio "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "Desafio de estagio"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [aysylu/loom "0.5.0"]]
   :main ^:skip-aot desafio.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/desafio/core.clj
+++ src/desafio/core.clj
@@ -5,3 +5,12 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+(def answer ":(")
+
+(def hai "O HAI!")
+
+(defn square [x]
+  ":(")
+
+(defn average [a b]
+  ":(")
